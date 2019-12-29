e24370bcc9b3cc6d2861d990875e81f95bd56485
kportalatin <kportalatin@corelogic.com>
2019-12-29 14:20:07 -0600

    commit

---
+++ CloudProjectIntellij/udemy-oop/src/main/java/com/udemy/oop/animal/Animal.java
@@ -0,0 +1,4 @@
+package com.udemy.oop.animal;
+
+public class Animal {
+}
---
+++ CloudProjectIntellij/udemy-oop/src/main/java/com/udemy/oop/human/Earth.java
@@ -0,0 +1,14 @@
+package oop.udemy.oop.human;
+
+public class Earth {
+    public static void main(String[] args) {
+
+        Human humanTom = new Human(5, "Tom", 42, "brown");
+        Human humanJoe = new Human(32, "Joe", 70, "green");
+        Human humanLynn = new Human(32, "Lynn", 63, "brown");
+
+        humanTom.speak();
+        humanJoe.speak();
+        humanLynn.speak();
+    }
+}
